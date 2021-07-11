c8f5a93d54674587d4756e4ae69420301e377b85
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-11 11:04:54 +0000

    Home

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -10,5 +10,13 @@ export const Name = () => {
     console.log("store", store.data);
     return (
             <input onChange={e => setState(e.target.value)} />
-    )
-}
+            <button onClick={() => actions.updateArray(state)}>Update</button>
+
+            {store.data.map((e, index) => (
+                <div key={index} className="text-center mt-5">
+                    <p>{e}</p>
+                    <button naming={e} style={"btn btn-success"} index={index} />
+                </div>
+            ))}
+    );
+};
