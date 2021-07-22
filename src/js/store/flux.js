1ab32cb6a7b6c2b9800ed4792e390e85902cf5ff
kevincastro015 <kevin.castro015@mymdc.net>
2021-07-22 20:47:54 +0000

    updateTodoAdded

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -31,17 +31,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 						"Content-Type": "application/json"
 					}
 				}).then(() => getActions().getTodos());
-				// let obj = { label: newData, done: false };
-				// let data = getStore().data;
-				// setStore({ data: [...data, obj] });
-				// console.log(getStore().data);
 			},
-			deleteElement: position => {
-				// let data = getStore().data;
-				// let newArray = data.filter((item, index) => position !== index);
+			deleteElement: newArray => {
 				fetch("https://assets.breatheco.de/apis/fake/todos/user/kevincastro015", {
 					method: "PUT", //or "POST"
-					body: JSON.stringify(position), // data can be 'string' or {object}!
+					body: JSON.stringify(newArray), // data can be 'string' or {object}!
 					headers: {
 						"Content-Type": "application/json"
 					}
@@ -52,7 +46,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				data[index] = { label: label, done: false };
 				fetch("https://assets.breatheco.de/apis/fake/todos/user/kevincastro015", {
 					method: "PUT", //or "POST"
-					body: JSON.stringify([data[index]]), // data can be 'string' or {object}!
+					body: JSON.stringify(data), // data can be 'string' or {object}!
 					headers: {
 						"Content-Type": "application/json"
 					}
